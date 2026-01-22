1780a6065cd6957a82616e611438b1eb0bbb72f5
ApunkFebriAlFurqon <apunkpunk1@gmail.com>
2026-01-22 22:21:45 +0700

    Menambahkan tampilan awal Huffman dan Dijkstra

---
+++ script.js
@@ -0,0 +1,13 @@
+function pilihAlgoritma(algoritma) {
+    if (algoritma === "huffman") {
+        alert(
+            "Kamu memilih Huffman Coding.\n\n" +
+            "Algoritma ini digunakan untuk kompresi data."
+        );
+    } else if (algoritma === "dijkstra") {
+        alert(
+            "Kamu memilih Algoritma Dijkstra.\n\n" +
+            "Algoritma ini digunakan untuk mencari jalur terpendek."
+        );
+    }
+}
